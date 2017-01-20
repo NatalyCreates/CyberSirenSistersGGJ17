4f1bec63e7143405a54447995b3ff658814edb41
sasham1 <sasham1@gmail.com>
2017-01-20 15:50:29 +0200

    WIP

--- Assets/DummyWaveGeneratorScript.cs
+++ Assets/DummyWaveGeneratorScript.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DummyWaveGeneratorScript : MonoBehaviour {
+    public enum WaveColor { None, Red, Blue, Yellow, Purple };
+    // Use this for initialization
+    void Start() {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     public void ReceiveInput()
     {
         Debug.Log("Received input - " + this.name);
+    }
+
+    public WaveColor GetWave()
+    {
+        if (Random.value < 0.1)
+            return WaveColor.Red;
+        else
+            return WaveColor.None;
     }
 }
