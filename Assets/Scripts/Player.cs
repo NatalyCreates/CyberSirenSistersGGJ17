32a57742545e8af1d790324b002c30bde8fd2524
sasham1 <sasham1@gmail.com>
2017-01-20 19:40:06 +0200

    fixed build

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-  List<float> samples;
-  KeyCode listenTo;
+    List<float> samples;
+    KeyCode listenTo = KeyCode.LeftShift;   // TODO make configurable
 
 	void Start () {
         samples = new List<float>();
-  }
+    }
 
 	void Update () {
-      UpdateArray(Input.GetKeyDown(listenTo))
-  }
+        UpdateArray(Input.GetKeyDown(listenTo));
+    }
 
-  private void UpdateArray(bool isClick) {
-      if (isClick) {
-          samples.Add(Time.time);
-      }
-      var index = samples.FindIndex(time => time < (Time.time - GameBalance.Instance.windowSeconds)
-      if (index > -1) {
-          samples.RemoveRange(0, index)
-      }
-  }
+    private void UpdateArray(bool isClick)
+    {
+        if (isClick) {
+            samples.Add(Time.time);
+        }
+        int index = samples.FindIndex(time => time < (Time.time - GameBalance.Instance.windowSeconds));
+        if (index > -1) {
+            samples.RemoveRange(0, index);
+        }
+    }
 
-  public float GetCurrentFrequency()
-  {
-      return samples.Count / GameBalance.Instance.windowSeconds;
-  }
+    public float GetCurrentFrequency()
+    {
+        return samples.Count / GameBalance.Instance.windowSeconds;
+    }
 
-  // animate siren sprite
+    // animate siren sprite
 }
