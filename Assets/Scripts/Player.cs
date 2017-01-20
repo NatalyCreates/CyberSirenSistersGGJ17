0d24514a4e6935204174878d7c4dd2135c4b6784
sasham1 <sasham1@gmail.com>
2017-01-20 19:56:41 +0200

    .

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -31,5 +31,20 @@ public class Player : MonoBehaviour {
         return samples.Count / GameBalance.Instance.windowSeconds;
     }
 
+    public GameBalance.FreqColor GetCurrentWave()
+    {
+        GameBalance.Instance.ColorThresholds[0] = 1.0f;
+        float freq = GetCurrentFrequency();
+        for(int i=0; i<GameBalance.Instance.ColorThresholds.Length && i<GameBalance.Instance.freqColors.Length; i++)
+        {
+            // Find the frequency 
+            if(freq < GameBalance.Instance.ColorThresholds[i])
+            {
+                return GameBalance.Instance.freqColors[i];
+            }
+        }
+        // Return the last color
+        return GameBalance.Instance.freqColors[GameBalance.Instance.freqColors.Length - 1];
+    }
     // animate siren sprite
 }
