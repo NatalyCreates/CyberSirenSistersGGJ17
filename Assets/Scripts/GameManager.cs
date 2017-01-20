05ea7983a19d614b5b8b837c5b7cfbc2e64b252b
NatalyCreates <nataly@natalycreates.com>
2017-01-20 21:43:45 +0200

    mmmmmmmmmmmmmm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,17 +9,6 @@ public class GameManager : MonoBehaviour {
     public GameObject playerLeft;
     public GameObject playerRight;
 
-    public AudioSource baseLayer;
-    public AudioSource baseLayerWaves;
-    public AudioSource layer1;
-    public AudioSource layer2;
-    public AudioSource layer3a;
-    public AudioSource layer3b;
-    public AudioSource layer4;
-    public AudioSource layer5;
-    public AudioSource layer6;
-    public AudioSource layer7;
-
     public GameObject shipPrefab;
 
     void Awake () {
@@ -41,7 +30,7 @@ public class GameManager : MonoBehaviour {
 
     void CreateShip()
     {
-        Vector3 shipPos = new Vector3(0, 0, 0);
+        Vector3 shipPos = new Vector3(0, 200, 0);
         GameObject newShip = Instantiate(shipPrefab, shipPos, Quaternion.identity) as GameObject;
         // init a prefab ship with random ShipType
         // also random position from list
@@ -64,67 +53,6 @@ public class GameManager : MonoBehaviour {
         
         // create a wave object with the color of that freq in the right/left corner
 
-    }
-
-    void ManageMusic(float leftFreq, float rightFreq)
-    {
-
-        // music
-
-        GameBalance.FreqColor color1 = GameBalance.Instance.GetColor(leftFreq);
-        GameBalance.FreqColor color2 = GameBalance.Instance.GetColor(rightFreq);
-        
-        GameBalance.FreqColor[] coloredPair = GetOrderedColorPair(color1, color2);
-
-        if (coloredPair[0] == GameBalance.FreqColor.White)
-        {
-            //baseMusic.volume = 1f;
-        }
-        else if (coloredPair[0] == GameBalance.FreqColor.Blue)
-        {
-            // base sound + first layer
-        }
-        else if ((coloredPair[0] == GameBalance.FreqColor.Yellow) && (coloredPair[1] <= GameBalance.FreqColor.Blue))
-        {
-            // next layer
-        }
-        else if ((coloredPair[0] == GameBalance.FreqColor.Yellow) && (coloredPair[1] == GameBalance.FreqColor.Yellow))
-        {
-            // next layer
-        }
-        else if ((coloredPair[0] == GameBalance.FreqColor.Red) && (coloredPair[1] <= GameBalance.FreqColor.Blue))
-        {
-            // next layer
-        }
-        else if ((coloredPair[0] == GameBalance.FreqColor.Red) && (coloredPair[1] <= GameBalance.FreqColor.Yellow))
-        {
-            // next layer
-        }
-        else if ((coloredPair[0] == GameBalance.FreqColor.Red) && (coloredPair[1] == GameBalance.FreqColor.Red))
-        {
-            // next layer
-        }
-    }
-
-    GameBalance.FreqColor[] GetOrderedColorPair(GameBalance.FreqColor color1, GameBalance.FreqColor color2)
-    {
-        GameBalance.FreqColor[] orderedPair = new GameBalance.FreqColor[2];
-        if (color1 == color2)
-        {
-            orderedPair[0] = color1;
-            orderedPair[1] = color2;
-        }
-        else if (color1 > color2)
-        {
-            orderedPair[0] = color1;
-            orderedPair[1] = color2;
-        }
-        else
-        {
-            orderedPair[0] = color2;
-            orderedPair[1] = color1;
-        }
-        return orderedPair;
     }
 
     public void LoseGame()
