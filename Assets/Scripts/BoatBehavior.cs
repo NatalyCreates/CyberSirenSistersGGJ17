4f9d9bb27474bf022be709f0d9a57b11a25beaab
NatalyCreates <nataly@natalycreates.com>
2017-01-21 13:59:15 +0200

    sink ship effect

--- Assets/Scripts/BoatBehavior.cs
+++ Assets/Scripts/BoatBehavior.cs
@@ -35,6 +35,9 @@ public class BoatBehavior : MonoBehaviour {
             // sink
             // animate
             // kill ship
+
+            MusicManager.Instance.PlaySunkShipEffect();
+
             AnimateSinking();
             if (!scoreUpdated)
             {
